fc09cedfe761ed93a2ffef13c421e319ac53f096
Lagarux <farukguner42@gmail.com>
2023-02-17 00:04:01 +0300

    Added project file and folders

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EFCategoryRepository EFCR;
+
+        public CategoryManager()
+        {
+            EFCR= new EFCategoryRepository();
+        }
+        public void AddCategory(Category category)
+        { 
+           EFCR.Insert(category);
+        }
+
+        public Category GetCategoryById(int id)
+        {
+           return EFCR.GetByID(id);
+        }
+
+        public List<Category> GetCategoryList()
+        {
+           return EFCR.GetListAll();
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            EFCR.Delete(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            EFCR.Update(category);
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    internal class CommentRepository : IGenericDal<Comment>
+    {
+        Context c=new Context();
+        public void Delete(Comment t)
+        {
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public Comment GetByID(int id)
+        {
+            return c.Comments.Find(id);
+        }
+
+        public List<Comment> GetListAll()
+        {
+            return c.Comments.ToList();
+        }
+
+        public void Insert(Comment t)
+        {
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void Update(Comment t)
+        {
+            c.Update(t);
+            c.SaveChanges();
+        }
+    }
+}
